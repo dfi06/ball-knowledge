95063605afe6bbbf6894841e556ee95c508b19bd
daffa ismail <daffa.ismail@cs.ui.ac.id>
2025-09-08 13:47:15 +0700

    Tugas 2 FIXX

--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Audemar Pidgeot Ronald',
-        'price': '$50m'
+        'name': 'Daffa Ismail',
+        'kelas': 'A'
     }
 
     return render(request, "main.html", context)
