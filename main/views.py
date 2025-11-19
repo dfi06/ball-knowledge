d2020d98696d802a62d9dcab7e7113027978f31f
daffa ismail <daffa.ismail@cs.ui.ac.id>
2025-11-19 21:14:06 +0700

    small fixes

--- main/views.py
+++ main/views.py
@@ -209,25 +209,25 @@ def proxy_image(request):
     
 
 @csrf_exempt
-def create_news_flutter(request):
+def create_product_flutter(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-        title = strip_tags(data.get("title", ""))  # Strip HTML tags
-        content = strip_tags(data.get("content", ""))  # Strip HTML tags
+        name = strip_tags(data.get("name", ""))  # Strip HTML tags
+        description = strip_tags(data.get("description", ""))  # Strip HTML tags
         category = data.get("category", "")
         thumbnail = data.get("thumbnail", "")
         is_featured = data.get("is_featured", False)
         user = request.user
         
-        new_news = Product(
-            title=title, 
-            content=content,
+        new_product = Product(
+            name=name, 
+            description=description,
             category=category,
             thumbnail=thumbnail,
             is_featured=is_featured,
             user=user
         )
-        new_news.save()
+        new_product.save()
         
         return JsonResponse({"status": "success"}, status=200)
     else:
